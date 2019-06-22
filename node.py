f67bc5a0b270bc66680f40ed9f87fe485a49bcd0
stefian <v_ianta@yahoo.com>
2019-06-22 08:56:13 -0400

    added the /chain route

--- node.py
+++ node.py
@@ -1,15 +1,22 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask_cors import CORS
 
 from wallet import Wallet
+from blockchain import Blockchain
 
 app = Flask(__name__)
 wallet = Wallet()
+blockchain = Blockchain(wallet.public_key)
 CORS(app)
 
 @app.route('/', methods=['GET'])
 def get_ui():
     return 'This works!'
+
+@app.route('/chain', methods=['GET'])
+def get_chain():
+    chain_snapshot = blockchain.chain
+    return jsonify(chain_snapshot), 200
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5010)
