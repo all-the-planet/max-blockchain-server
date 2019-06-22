6fa607f73deeed7553fc2f90c3d1889aee2bf3e7
stefian <v_ianta@yahoo.com>
2019-06-22 09:13:19 -0400

    fixed the jsonify issue by adding a dictionary for transactions in a block

--- node.py
+++ node.py
@@ -16,7 +16,10 @@ def get_ui():
 @app.route('/chain', methods=['GET'])
 def get_chain():
     chain_snapshot = blockchain.chain
-    return jsonify(chain_snapshot), 200
+    dict_chain = [block.__dict__.copy() for block in chain_snapshot]
+    for dict_block in dict_chain:
+        dict_block['transactions'] = [tx.__dict__ for tx in dict_block['transactions']]
+    return jsonify(dict_chain), 200
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5010)
