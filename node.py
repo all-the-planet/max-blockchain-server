189d4d52531d5b42fe22fb309a0c86cfa8b50b8a
stefian <v_ianta@yahoo.com>
2019-06-22 00:02:41 -0400

    first commit on the blockchain API server

---
+++ node.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask_cors import CORS
+
+from wallet import Wallet
+
+app = Flask(__name__)
+wallet = Wallet()
+CORS(app)
+
+@app.route('/', methods=['GET'])
+def get_ui():
+    return 'This works!'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5010)
+
