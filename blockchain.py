0a0991242aabcb544d8210048c62b99ab4eff1c9
stefian <stefan.ianta@w3ai.net>
2019-06-27 11:42:10 -0300

    formatted block.py according to PEP 8

--- blockchain.py
+++ blockchain.py
@@ -154,10 +154,11 @@ class Blockchain:
         print(tx_sender)
         amount_sent = reduce(lambda tx_sum, tx_amt: tx_sum + sum(tx_amt)
                              if len(tx_amt) > 0 else tx_sum + 0, tx_sender, 0)
-        # This fetches received coin amounts of transactions that were already 
+        # This fetches received coin amounts of transactions that were already
         # included in blocks of the blockchain
-        # We ignore open transactions here because you shouldn't be able to 
-        # spend coins before the transaction was confirmed + included in a block
+        # We ignore open transactions here because you shouldn't be able to
+        # spend coins before the transaction was confirmed + included 
+        # in a block
         tx_recipient = [[tx.amount for tx in block.transactions
                          if tx.recipient == participant] for block in self.__chain]
         amount_received = reduce(lambda tx_sum, tx_amt: tx_sum + sum(tx_amt)
